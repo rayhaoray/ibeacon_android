4bb129503f57160272f830a06572f5325dc552b4
Zhenhao Lei <zhenhaolei@sessionm.com>
2014-07-28 11:32:29 -0400

    Updated TxPower constant

--- src/com/example/bluetooth/le/DeviceControlActivity.java
+++ src/com/example/bluetooth/le/DeviceControlActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.bluetooth.le;
 
 import java.text.DecimalFormat;
@@ -57,7 +41,7 @@ public class DeviceControlActivity extends Activity {
     public static final String EXTRAS_DEVICE_TYPE = "DEVICE_IBEACON_TYPE";
 
     // default 1m strength
-    private static final int TXPOWER = -50;
+    private static final int TXPOWER = -49;
 
     private TextView mConnectionState;
     private TextView mUUIDField;
